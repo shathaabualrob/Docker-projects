f6e4173c45a8dcb398f1bf72c5c2b5a3126efdd5
Shatha AR <shatharub5@gmail.com>
2023-01-07 12:01:23 +0200

    Finishing up example2

--- example2/venv/Scripts/activate.bat
+++ example2/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Lenovo\OneDrive\Desktop\django\Docker-projects\example2\venv
+set VIRTUAL_ENV=C:\Users\shath\OneDrive\Desktop\django\docker-projects\example2\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,6 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(venv) 
 
 :END
 if defined _OLD_CODEPAGE (
